3cec40bf863f219a0096dd923950f60f8feb8012
Mark Guiang <mahikgot@gmail.com>
2022-02-04 03:29:28 +0800

    separate outlet into div

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,18 @@ import { Outlet } from 'react-router-dom';
 import HeaderButton from './HeaderButton';
 
 const Header = () => (
-  <div className="flexContainerHome">
-    <div className="leftHome">
-      <h1 className="nameHome">
-        Kat, 22
-      </h1>
-      <p>
-        Heavily influenced by 2014 Tumblr and a shameless enjoyer of most mainstream things
-      </p>
+  <div>
+    <div className="flexContainerHome">
+      <div className="leftHome">
+        <h1 className="nameHome">
+          Kat, 22
+        </h1>
+        <p>
+          Heavily influenced by 2014 Tumblr and a shameless enjoyer of most mainstream things
+        </p>
+      </div>
+      <HeaderButton />
     </div>
-    <HeaderButton />
     <Outlet />
   </div>
 );
