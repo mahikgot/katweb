870ea02e01e14681683435bcf9b92c3ac6dbbb18
unknown <mahikgot@gmail.com>
2023-02-11 13:53:33 +0800

    configure for use in bucket

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Header from './components/Header';
 import './App.css';
 
 const App = () => (
-  <div className="overlay">
-    <Routes>
-      <Route path="/" element={<Header />}>
-        <Route path="works" />
-        <Route path="about" />
-      </Route>
-    </Routes>
-  </div>
+<div className="overlay">
+        <Routes>
+            <Route path="/" element={<Header />}>
+                <Route path="works" />
+                <Route path="about" />
+            </Route>
+        </Routes>
+</div>
 );
 export default App;
